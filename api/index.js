a3e08baeb3c9314f1386d2d094d7b31144178d52
vishrutpanya <vishrutpanya03@gmail.com>
2024-02-09 16:49:55 +0530

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import authRouter from './routes/auth.route.js';
 import userRouter from './routes/user.route.js';
 import cookieParser from 'cookie-parser';
+import listingRouter from './routes/listing.route.js'
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.listen(3000, () => {
 
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
+app.use('/api/listing', listingRouter);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
