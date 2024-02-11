e6e3aa030ea271d545d89a081b7a947cefc79012
vishrutpanya <vishrutpanya03@gmail.com>
2024-02-11 16:12:07 +0530

    complete listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './Pages/CreateListing'
 import UpdateListing from './Pages/UpdateListing'
+import Listing from './Pages/listing'
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
         <Route path='/sign-in' element = {<SignIn />} />
         <Route path='/sign-up' element = {<SignUp />} />
         <Route path='/about' element = {<About />} />
+        <Route path='/listing/:listingId' element = {<Listing />} />
 
         <Route element = {<PrivateRoute />}>
         <Route path='/profile' element = {<Profile />} />
