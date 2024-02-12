954735aacf0cf6dda08021b194a5d75b914ec81b
vishrutpanya <vishrutpanya03@gmail.com>
2024-02-12 11:26:52 +0530

    Complete listing page functionality

--- client/src/Pages/Listing.jsx
+++ client/src/Pages/Listing.jsx
@@ -14,7 +14,7 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
-
+import Contact from '../components/Contact';
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
@@ -134,9 +134,18 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
-            </div>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+              >
+                Contact landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing} />}
+          </div>
         </div>
       )}
     </main>
-    );
+  );
 }
