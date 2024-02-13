3a52c6e3f1b82cbea6caf00f8b3333b3df6f6132
vishrutpanya <vishrutpanya03@gmail.com>
2024-02-14 01:50:20 +0530

    profile page error resolve

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { useRef, useState, useEffect } from 'react';
 import {getDownloadURL, getStorage,ref,  uploadBytesResumable} from 'firebase/storage';
 import { app } from '../firebase';
-import {updateUserStart,updateUserSuccess,updateUserFailure, deleteUserFailure,deleteUserStart,deleteUserSuccess, signOutUserStart, signOutUserSuccess, signOutUserFailure} from '../redux/user/userSlice';
+import {updateUserStart,updateUserSuccess,updateUserFailure, deleteUserFailure,deleteUserStart,deleteUserSuccess, signOutUserStart, signOutUserSuccess, signOutUserFailure} from '../redux/User/userSlice';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
