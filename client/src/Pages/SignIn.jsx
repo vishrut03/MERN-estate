1b710c7d976e84052ba276cc58b21e8e28df81a2
vishrutpanya <vishrutpanya03@gmail.com>
2024-02-14 01:33:09 +0530

    resolve signin.jsx issue

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice.js';
 import OAuth from '../components/OAuth';
 
 export default function SignIn() {
