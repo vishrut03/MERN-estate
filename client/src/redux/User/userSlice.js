b9afdd843407547a255c55a571fdb123a967e0ce
vishrutpanya <vishrutpanya03@gmail.com>
2024-02-14 01:45:42 +0530

    userSlice issue resolved

--- client/src/redux/User/userSlice.js
+++ client/src/redux/User/userSlice.js
@@ -61,7 +61,19 @@ const userSlice = createSlice({
   },
 });
 
-export const {signInStart, signInSuccess, signInFailure, updateUserFailure, updateUserSuccess, updateUserStart, deleteUserFailure,
-  deleteUserSuccess, deleteUserStart, signOutUserFailure, signOutUserSuccess, signOutUserStart} = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateUserFailure,
+  updateUserSuccess,
+  updateUserStart,
+  deleteUserFailure,
+  deleteUserSuccess,
+  deleteUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
+  signOutUserStart,
+} = userSlice.actions;
 
 export default userSlice.reducer;
