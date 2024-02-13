7c29c2cc018db106aeba88def2079c5405eb519a
vishrutpanya <vishrutpanya03@gmail.com>
2024-02-13 19:51:23 +0530

    Add show more functionality

--- client/src/Pages/Search.jsx
+++ client/src/Pages/Search.jsx
@@ -16,6 +16,7 @@ export default function Search() {
     
       const [loading, setLoading] = useState(false);
       const [listings, setListings] = useState([]);
+      const [showMore, setShowMore] = useState(false);
       const navigate = useNavigate();
       //console.log(listings);
     
@@ -51,9 +52,15 @@ export default function Search() {
     
         const fetchListings = async () => {
           setLoading(true);
+          setShowMore(false);
           const searchQuery = urlParams.toString();
           const res = await fetch(`/api/listing/get?${searchQuery}`);
           const data = await res.json();
+          if (data.length > 8) {
+            setShowMore(true);
+          } else {
+            setShowMore(false);
+          }
           setListings(data);
           setLoading(false);
         };
@@ -107,6 +114,20 @@ export default function Search() {
         urlParams.set('order', sidebardata.order);
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`);
+      };
+
+      const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if (data.length < 9) {
+          setShowMore(false);
+        }
+        setListings([...listings, ...data]);
       };
 
 
@@ -230,6 +251,15 @@ export default function Search() {
             listings.map((listing) => (
               <ListingItem key={listing._id} listing={listing} />
             ))}
+
+            {showMore && (
+            <button
+              onClick={onShowMoreClick}
+              className='text-green-700 hover:underline p-7 text-center w-full'
+            >
+              Show more
+            </button>
+          )}
        </div>
       </div>
     </div>
