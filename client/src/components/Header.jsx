ef604a6f1a50cd54f526cae647cddbd00e0c24a8
vishrutpanya <vishrutpanya03@gmail.com>
2024-07-05 23:00:59 +0530

    Header CSS fixed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
     }
   }, [location.search]);
   return (
-    <header className="bg-slate-200 shadow-md">
+    <header className="top-0 sticky w-full z-30 bg-slate-200 shadow-md border-y-2 border-slate-300 ${HeaderCSS.head}">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
