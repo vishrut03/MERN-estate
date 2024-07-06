ad2a470da8d6eada0e5de2cdaf6ac10410321999
vishrutpanya <vishrutpanya03@gmail.com>
2024-07-06 18:49:17 +0530

    minor changes

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,14 @@
-import express from 'express';
-import { updateUser, deleteUser, getUserListings, getUser } from '../controller/user.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
-
+import express from "express";
+import {
+  updateUser,
+  getUserListings,
+  getUser,
+} from "../controller/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
-router.post('/update/:id', verifyToken, updateUser)
-router.delete('/delete/:id', verifyToken, deleteUser)
-router.get('/listings/:id', verifyToken, getUserListings)
-router.get('/:id', verifyToken, getUser)
+router.post("/update/:id", verifyToken, updateUser);
+router.get("/listings/:id", verifyToken, getUserListings);
+router.get("/:id", verifyToken, getUser);
 
 export default router;
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,41 +1,48 @@
-import React from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
-import { app } from '../firebase';
-import { useDispatch } from 'react-redux';
-import { signInSuccess } from '../redux/User/userSlice';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/User/userSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
-      const result = await signInWithPopup(auth, provider);      
+      const result = await signInWithPopup(auth, provider);
 
-      const res = await fetch('/api/auth/google', {
-        method: 'POST',
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            name: result.user.displayName,
-            email: result.user.email,
-            photo: result.user.photoURL,
-          }),
-      })
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
 
       const data = await res.json();
       dispatch(signInSuccess(data));
-      navigate('/');
+      navigate("/profile");
     } catch (error) {
-      console.log('could not sign in with google', error);
+      console.log("could not sign in with google", error);
     }
-  }
+  };
 
   return (
-    <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'> Continue With Google </button>
+    <button
+      onClick={handleGoogleClick}
+      type="button"
+      className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
+    >
+      {" "}
+      Continue With Google{" "}
+    </button>
   );
 }
