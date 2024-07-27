10e49aaa602df894bacf81cfd254b6cb05731639
vishrutpanya <vishrutpanya03@gmail.com>
2024-07-27 13:53:04 +0530

    frontend_changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "import.meta.env.BACKEND_URL",
         secure: false,
       },
     },
   },
   plugins: [react()],
-})
+});
