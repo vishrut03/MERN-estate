de5f2b05d02e77fd064aa1238225644f842c6966
vishrutpanya <vishrutpanya03@gmail.com>
2024-07-27 15:18:54 +0530

    frontend_viteconfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "import.meta.env.BACKEND_URL",
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // Load env file based on the mode (development, production, etc.)
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL, // Use the loaded environment variable
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  };
 });
