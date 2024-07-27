5bb86141b5277e8c028f651c062f68bd74c914bc
vishrutpanya <vishrutpanya03@gmail.com>
2024-07-27 15:52:15 +0530

    backend_changes

--- api/index.js
+++ api/index.js
@@ -1,12 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 // import path from "path";
 dotenv.config();
+
+const corsOptions = {
+  origin: "https://mern-estate-frontend-three.vercel.app/", // replace with your actual frontend domain
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 mongoose
   .connect(process.env.MONGO)
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 export default defineConfig(({ mode }) => {
   // Load env file based on the mode (development, production, etc.)
